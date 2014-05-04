c0765de6b38fba696d337899d8ec80f31d1e88ad
Smile Pabby <smile.pabby@gmail.com>
2014-05-04 11:51:09 +0530

    Initail commit

---
+++ AmazonTechgig/src/CandidateCode.java
@@ -0,0 +1,12 @@
+public class CandidateCode 
+{ 
+    public static int[] arraySum(int[] input1,int[] input2)
+    {
+	int[] output=new int[input1.length];
+        for(int i=0;i<input1.length;i++)
+        {
+            output[i]=input1[i]+input2[i];
+        }
+        return output;
+    }
+}
